0fec0da83b67e5d0aaefc6be73fe1151653d73e6
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-12 12:31:07 -0400

    Querys added.

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,12 +1,33 @@
 ﻿
+using System;
+using System.Linq;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var context = new PlutoContext())
+            {
+                //Extension methods syntax
+                var courses = context.Courses
+                    .Where(c => c.Name.Contains("C#"))
+                    .OrderBy(c => c.Name)
+                    .ToList();
 
+                foreach (var course in courses) 
+                    Console.WriteLine(course.Name);
 
+                //LINQ Syntax
+                var coursesQuery = from c in context.Courses
+                    where c.Name.Contains("C#")
+                    orderby c.Name
+                    select c;
+
+                foreach (var course in coursesQuery) 
+                    Console.WriteLine(course.Name);
+            }
         }
     }
 }
